39385d15aae008aa64544b4b4f79f91feb181e63
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-13 01:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-//complete this code
-class Animal {
+// //complete this code
+// class Animal {
+// 	constructor(species){
+// 		this._species=species;
+// 	}
+
+// 	get species(){
+// 		return this._species;
+// 	}
+
+// 	makeSound(){
+// 		console.log(`The ${this.species} makes a sound`)
+// 	}
+// }
+
+// class Dog extends Animal {
+// 	bark(){
+// 		console.log("woof");
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	purr(){
+// 		console.log("purr");
+// 	}
+// }
+
+class Animal{
 	constructor(species){
 		this._species=species;
 	}
-
 	get species(){
 		return this._species;
 	}
-
 	makeSound(){
-		console.log(`The ${this.species} makes a sound`)
+		console.log(`The ${this._species} makes a sound`);
 	}
 }
 
-class Dog extends Animal {
-	bark(){
-		console.log("woof");
-	}
-}
-
-class Cat extends Animal {
+class Cat extends Animal{
 	purr(){
 		console.log("purr");
+	}
+}
+
+class Dog extends Animal{
+	bark() {
+		console.log("woof");
 	}
 }
 
